7a6fc691275cf6939bf56844158875ba56e92f34
bezawitsebsbe <167539304+bezawitsebsbe@users.noreply.github.com>
2025-07-31 18:49:18 +0300

    Time: 579 ms (58.28%) | Memory: 20.2 MB (96.76%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,32 +1,42 @@
 class Solution:
   def threeSum(self, nums: list[int]) -> list[list[int]]:
     if len(nums) < 3:
-      return []
+        return []
 
     ans = []
-
     nums.sort()
 
     for i in range(len(nums) - 2):
-      if i > 0 and nums[i] == nums[i - 1]:
-        continue
-      # Choose nums[i] as the first number in the triplet, then search the
-      # remaining numbers in [i + 1, n - 1].
-      l = i + 1
-      r = len(nums) - 1
-      while l < r:
-        summ = nums[i] + nums[l] + nums[r]
-        if summ == 0:
-          ans.append((nums[i], nums[l], nums[r]))
-          l += 1
-          r -= 1
-          while nums[l] == nums[l - 1] and l < r:
-            l += 1
-          while nums[r] == nums[r + 1] and l < r:
-            r -= 1
-        elif summ < 0:
-          l += 1
-        else:
-          r -= 1
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+        l = i + 1
+        r = len(nums) - 1
 
-    return ans       
+        while l < r:
+            summ = nums[i] + nums[l] + nums[r]
+
+            if summ == 0:
+                ans.append((nums[i], nums[l], nums[r]))
+                l += 1
+                r -= 1
+                while l < r and nums[l] == nums[l - 1]:  # Avoid duplicates
+                    l += 1
+                while l < r and nums[r] == nums[r + 1]:  # Avoid duplicates
+                    r -= 1
+            elif summ < 0:
+                l += 1
+            else:
+                r -= 1
+
+    return ans               
+
+            
+
+
+
+
+
+
+
+    
+        
